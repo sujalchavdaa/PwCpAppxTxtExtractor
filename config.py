2d466964f9b255a67c41a5258a385c950ea54ca8
sujalchavdaa <sujalchavda500@gmail.com>
2025-06-05 09:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "26330942"
+api_hash = "5de9fd033aa828dfd3bf0c28adeee660"
+bot_token = "7174037288:AAEU2jsOmC1LdpnJQ5qXKdM5caHPZjSO7dI"
+auth_users = [6883471516]
